29f203a4145c2e052e357ac4b5c2735eefcec36f
envkv <88292180+envkv@users.noreply.github.com>
2024-11-16 16:59:53 +0300

    Delete main_page directory

--- main_page/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-
-class Task(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    completed = models.BooleanField(default=False)
